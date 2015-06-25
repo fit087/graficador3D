b05696d28cfc8df166379ae8583617b3e66bafcd
Adolfo Correa <fitomillennium@hotmail.com>
2015-06-25 10:12:31 -0300

    Implementation to plot the contactmesh

--- graficador3D.m
+++ graficador3D.m
@@ -1,4 +1,10 @@
 % load  './datos h200 com\CA.mat'
 % load  'nuvem.mat'
 % plot3(matriz(:,1),matriz(:,2),matriz(:,3))
+% figure
 scatter3(matriz(:,1),matriz(:,2),matriz(:,3))
+hold on
+scatter3(matriz1(:,1),matriz1(:,2),matriz1(:,3), 'r')
+
+% scatter3(matriz(:,1),matriz(:,2),matriz(:,3),'b', matriz1(:,1),matriz1(:,2),matriz1(:,3), 'r')
+%scatter3(matriz1(:,1),matriz1(:,2),matriz1(:,3), 'r')
